232dca6973c194f5974dc6ec2ad715c2725729aa
lguaman <luis.guaman@fullsegura.com>
2024-07-19 14:09:01 -0500

    new line

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -9,20 +9,21 @@ const Features = () => {
         <div className="container">
           <SectionTitle
             title="Nuestros Productos"
-            paragraph="
-- Ear Cuffs: Adornos para la oreja sin necesidad de perforaciones.
-- Body Chains: Cadenas corporales que añaden un toque de estilo a tu outfit.
-- Pulseras: Desde estilos minimalistas hasta diseños más elaborados.
-- Aretes: Variedad de estilos que van desde lo clásico hasta lo contemporáneo.
-- Tobilleras: Detalles delicados y modernos.
-- Anillos de Uñas: Una tendencia audaz y moderna.
-- Anillos de Pie: Añade un toque de elegancia a tus pies.
-- Cadenas de Cabeza: Accesorios únicos, perfectos para eventos especiales.
-- Cadenas de Vientre: Realza tu figura con nuestras cadenas de vientre.
-- Cadenas de Espalda: Diseños sofisticados que añaden un toque de glamour a tu espalda.
-- Piercings: Variedad de delicados piercings que marcan tu estilo.
-- Soportes de Celulares de Diseño: Eleva tu estilo incluso en tu celular.
-"
+            paragraph={`
+              - Ear Cuffs: Adornos para la oreja sin necesidad de perforaciones.
+              - Body Chains: Cadenas corporales que añaden un toque de estilo a tu outfit.
+              - Pulseras: Desde estilos minimalistas hasta diseños más elaborados.
+              - Aretes: Variedad de estilos que van desde lo clásico hasta lo contemporáneo.
+              - Tobilleras: Detalles delicados y modernos.
+              - Anillos de Uñas: Una tendencia audaz y moderna.
+              - Anillos de Pie: Añade un toque de elegancia a tus pies.
+              - Cadenas de Cabeza: Accesorios únicos, perfectos para eventos especiales.
+              - Cadenas de Vientre: Realza tu figura con nuestras cadenas de vientre.
+              - Cadenas de Espalda: Diseños sofisticados que añaden un toque de glamour a tu espalda.
+              - Piercings: Variedad de delicados piercings que marcan tu estilo.
+              - Soportes de Celulares de Diseño: Eleva tu estilo incluso en tu celular.
+              `}
+
             center
           />
 
