6f3322b4e5039663d768b19f7acd233a2a21ffc0
lguaman <luis.guaman@fullsegura.com>
2024-07-20 10:59:06 -0500

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,8 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About Page | Free Next.js Template for Startup and SaaS",
-  description: "This is About Page for Startup Nextjs Template",
+  title: "Soie tu estilo",
+  description: "Soie su estilo.",
   // other metadata
 };
 
@@ -14,11 +14,15 @@ const AboutPage = () => {
   return (
     <>
       <Breadcrumb
-        pageName="About Page"
-        description="Lorem ff ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="Acerca"
+        description="Gracias por ser parte de nuestra comunidad, donde nos encanta complementar tu belleza, con piezas que expresan tu personalidad. 
+Nuestras colecciones están diseñadas para llevar tu estilo a otro nivel.
+En Soie Styling, te ofrecemos una experiencia de compra única:
+Atención al cliente personalizada: Te ayudaremos a encontrar la pieza perfecta 
+Envíos nacionales: Disfruta de nuestras joyas sin importar dónde te encuentres.
+Diseños exclusivos y en tendencia: Piezas únicas que se adaptan a todos los estilos.
+"
       />
-      <AboutSectionOne />
-      <AboutSectionTwo />
     </>
   );
 };
